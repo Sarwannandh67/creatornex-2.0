f46522543a7923ea945911ac3dad857b359d942b
[firstname <creatornex@example.com>
2025-05-22 23:38:37 +0530

    fix: font loading and MIME type issues

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Preload critical fonts */}
+        <link
+          rel="preload"
+          href="/fonts/Croogla4F-Medium.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/HarmoniaSansProCondensed-Regular.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
